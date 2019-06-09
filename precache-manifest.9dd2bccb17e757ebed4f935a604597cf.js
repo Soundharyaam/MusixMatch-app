824d0198f7c59a8049025f709080a49f43c83600
Soundharyaam <soundharyabharatiya@gmail.com>
2019-06-09 17:29:59 +0530

    Updates

---
+++ precache-manifest.9dd2bccb17e757ebed4f935a604597cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa0cae9cf0f4c64e1b45",
+    "url": "/MusixMatch-app/static/js/runtime~main.fa0cae9c.js"
+  },
+  {
+    "revision": "d973a3d1cf805436accb",
+    "url": "/MusixMatch-app/static/js/main.d973a3d1.chunk.js"
+  },
+  {
+    "revision": "ae849625a4a814f63607",
+    "url": "/MusixMatch-app/static/js/1.ae849625.chunk.js"
+  },
+  {
+    "revision": "d973a3d1cf805436accb",
+    "url": "/MusixMatch-app/static/css/main.6b87c6f3.chunk.css"
+  },
+  {
+    "revision": "ed9a6689ca676504f0bb62d8e46f5750",
+    "url": "/MusixMatch-app/index.html"
+  }
+];
